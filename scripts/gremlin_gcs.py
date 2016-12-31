01575c6d2c42c7e6b8a7448e58fea4aebf832174
Sean Grady <vedicmonk@gmail.com>
2016-12-31 21:25:06 +0000

    added framework for gcs node

--- scripts/gremlin_gcs.py
+++ scripts/gremlin_gcs.py
@@ -4,5 +4,13 @@ import rospy
 
 
 class GremlinGCS:
-    def __ini__(self):
+    def __init__(self):
+        rospy.init_node('gremlin_gcs')
+        self.setup_subs()
+
+    def setup_subs(self):
         pass
+
+
+if __name__ == '__main__':
+    gcs = GremlinGCS()
