9b0b53b9ab801ad72bd602aa5ffa681c03fb1050
Sean Grady <vedicmonk@gmail.com>
2017-01-11 21:01:14 -0800

    refactored establish_database_connection

--- scripts/gremlin_mapserver.py
+++ scripts/gremlin_mapserver.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
+from database_functions import establish_database_connection
 
 
 class GremlinMapServer:
     def __init__(self):
         rospy.init_node('gremlin_mapserver')
-        establish_database_connection(self)
+        engine, Session = establish_database_connection()
+        self.engine = engine
+        self.Session = Session
         rospy.spin()
 
+
+if __name__ == '__main__':
+    ms = GremlinMapServer()
--- scripts/utils/database_functions.py
+++ scripts/utils/database_functions.py
@@ -1,6 +1,10 @@
-def establish_database_connection(obj):
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def establish_database_connection():
     """Setup database connection and sqlalchemy interface."""
     db_name = 'mission_data'
     db_url = 'mysql+mysqldb://root:password@localhost/' + db_name
-    obj.engine = create_engine(db_url)
-    obj.Session = sessionmaker(bind=self.engine)
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=self.engine)
+    return engine, Session
